480f8bb8c0002057c3590ea1ed7f296f2166a094
Victor Nunes <victor.marcelino.nunes@usp.br>
2016-09-22 03:08:20 -0300

    Icone criado

--- app/src/main/java/com/example/victor/fester/MainActivity.java
+++ app/src/main/java/com/example/victor/fester/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Fester! • Lista de Músicas");
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
--- app/src/main/java/com/example/victor/fester/MusicAddMenu.java
+++ app/src/main/java/com/example/victor/fester/MusicAddMenu.java
@@ -18,7 +18,11 @@ public class MusicAddMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_music_add_menu);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
